f7ec3e28f3812c84ad9d24dcdd55adf559f4e2b5
Reed Nelson-Saunders <reedns@hotmail.com>
2015-01-28 11:46:15 -0800

    add reservation test

--- test/features/reservations/making_a_reservation_test.rb
+++ test/features/reservations/making_a_reservation_test.rb
@@ -3,10 +3,11 @@ require 'test_helper'
 feature 'reserving an appointment' do
   scenario 'client reserves an available appointment' do
     sign_in(:client)
-    visit appointment_path(appointments(:available_appointment))
+    visit root_path
+    click_link "#{appointments(:available_appointment)}"
     fill_in 'Reservation Time', with: '2:00pm'
     click_button 'Reserve Appointment'
 
-    page.must_have_content 'Appointment was successfully updated.'
+    page.must_have_content 'The appointment was succesfully booked.'
   end
 end
