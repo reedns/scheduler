fe31fd1b00941014c23ebcc711e0eea532e8c9ea
Reed Nelson-Saunders <reedns@hotmail.com>
2017-04-08 20:11:23 -0700

    clean up code in appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,29 +2,26 @@ class Appointment < ActiveRecord::Base
   belongs_to :client, class_name: 'User'
   belongs_to :therapist, class_name: 'User'
 
-  validates :date, presence: true
-  validates :start_time, presence: true
-  validates :end_time, presence: true
+  validates :date, :start_time, :end_time, presence: true
   validates :reservation_time, presence: true, on: :update, if: :user_client?
   validate :reservation_valid, on: :update
-  validate :valid_block
+  validate :valid_time
+  validate :valid_date
+
+  private
 
   def reservation_valid
-    if reservation_time != nil
-      if reservation_time < start_time || reservation_time > end_time
-        errors.add(:reservation_time, 'must be within the available block')
-      end
+    if reservation_time && (reservation_time < start_time || reservation_time > end_time)
+      errors.add(:reservation_time, 'must be within the available block')
     end
   end
 
-  def valid_block
-    if start_time > end_time
-      errors.add(:start_time, 'must be before the start time')
-    end
+  def valid_time
+    errors.add(:start_time, 'must be before the start time') if start_time > end_time
+  end
 
-    if date <= Date.today
-      errors.add(:date, 'must be in the future')
-    end
+  def valid_date
+    errors.add(:date, 'must be in the future') if date <= Date.today
   end
 
   def user_client?
