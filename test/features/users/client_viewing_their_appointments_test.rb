022f5127a9e95f9429cf1d5d3c3684076e190e6d
Reed Nelson-Saunders <reedns@hotmail.com>
2017-04-13 14:47:05 -0700

    add placeholders for other user appointment list tests

--- test/features/users/client_viewing_their_appointments_test.rb
+++ test/features/users/client_viewing_their_appointments_test.rb
@@ -1,10 +1,14 @@
 require "test_helper"
 
-feature 'a client views their appointment list' do
+feature 'a user views their appointment list' do
   scenario 'a client can view their upcoming appointments' do
     sign_in(:client)
     visit user_appointments_path(users(:client))
 
     page.must_have_content 'Upcoming Appointments'
   end 
+
+  scenario 'a therapist can view their upcoming appointments'
+  scenario 'a client can view past appointments'
+  scenario 'a therapist can view past appointments'
 end
