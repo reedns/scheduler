76625dd7c158ee59695fe861ef2a7b4f4fd3c5b6
Reed Nelson-Saunders <reedns@hotmail.com>
2017-04-13 17:41:32 -0700

    setup tests for viewing past appointments

--- test/features/users/user_viewing_their_appointments_test.rb
+++ test/features/users/user_viewing_their_appointments_test.rb
@@ -6,9 +6,34 @@ feature 'a user views their appointment list' do
     visit user_appointments_path(users(:client))
 
     page.must_have_content 'Upcoming Appointments'
+    page.must_have_content users(:client).full_name
   end 
 
-  scenario 'a therapist can view their upcoming appointments'
-  scenario 'a client can view past appointments'
-  scenario 'a therapist can view past appointments'
+  scenario 'a therapist can view their upcoming appointments' do
+    sign_in(:therapist)
+    visit user_appointments_path(users(:therapist))
+
+    page.must_have_content 'Upcoming Appointments'
+    page.must_have_content users(:therapist).full_name
+  end
+
+  scenario 'a client can view past appointments' do
+    sign_in(:client)
+    visit user_appointments_path(users(:client), view: 'past_appointments')
+
+    page.must_have_content 'Past Appointments'
+    page.must_have_content users(:client).full_name
+  end
+
+  scenario 'a therapist can view past appointments' do
+    sign_in(:therapist)
+    visit user_appointments_path(users(:therapist))
+
+    page.must_have_content 'Past Appointments'
+    page.must_have_content users(:therapist).full_name
+  end
+
+
+  scenario "a client cannot see other client's or therapist's appointments"
+  scenario "a therapist cannot see other therapist's or a client's appointments"
 end
